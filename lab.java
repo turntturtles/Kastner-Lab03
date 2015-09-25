950a32b85405db4a5e094605a9da8094556bd8a9
unknown <4070132@HS222-08.vtsd.com>
2015-09-25 12:08:53 -0400

    Lab 03

---
+++ lab.java
@@ -0,0 +1,59 @@
+
+/**
+ * Write a description of class lab here.
+ * 
+ * Elise Kastner 
+ * 100 Point Version
+ */
+public class lab
+   // Lab03Dst.java
+// The Visible Average Program
+// This is the student, starting version, of Lab 03C.
+{
+	public static void main(String args[])
+	{
+		System.out.println("Lab 03D");
+		System.out.println();
+		int num1 = 15;
+		int num2 = 24;
+		int num3 = 36;
+		int num4 = 54;
+		int num5 = 62;
+		System.out.println("integer 1 = " + num1);
+		System.out.println("integer 2 = " + num2);
+		System.out.println("integer 3 = " + num3);
+		System.out.println("integer 4 = " + num4);
+		System.out.println("integer 5 = " +num5);
+		System.out.println("-------------------");
+		
+		int total = num1 + num2 + num3 + num4 + num5;
+		
+		int average = total / 5;
+		
+		System.out.println("Int total = " + total);
+		System.out.println("Int average = " + average);
+		System.out.println();
+		System.out.println();
+		double nm1 = 15.65;
+		double nm2 = 26.75;
+		double nm3 = 35.54;
+		double nm4 = 46.82;
+		double nm5 = 85.43;
+		System.out.println("1st real num = " + nm1);
+		System.out.println("2nd real num = " + nm2);
+		System.out.println("3rd real num = " + nm3);
+		System.out.println("4th real num = " + nm4);
+		System.out.println("5th real num = " + nm5);
+		System.out.println("----------------------");
+		
+		double all = nm1 + nm2 + nm3 + nm4 + nm5;
+		
+		double mean = total / 5;
+
+
+
+
+		System.out.println();
+
+		}
+}
